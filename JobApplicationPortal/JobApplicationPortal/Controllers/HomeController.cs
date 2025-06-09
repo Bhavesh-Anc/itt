eb0fc124ae5ca61e2afa61781721c2548e2d4198
Bhavesh Anchalia <anchaliabhavesh1@gmail.com>
2025-06-09 11:52:43 +0530

    Add files via upload

---
+++ JobApplicationPortal/JobApplicationPortal/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using JobApplicationPortal.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace JobApplicationPortal.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IJobService _jobService;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public HomeController(IJobService jobService, UserManager<ApplicationUser> userManager)
+        {
+            _jobService = jobService;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            if(User.Identity.IsAuthenticated)
+            {
+                var userId = _userManager.GetUserId(User);
+                var jobs = await _jobService.GetUserApplicationsAsync(userId);
+                return View(jobs);
+            }
+            return View();
+        }
+    }
+}

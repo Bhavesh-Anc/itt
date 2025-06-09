0d35ac64675e3bbe57d56e1128f4d8f501b1aa15
Bhavesh Anchalia <anchaliabhavesh1@gmail.com>
2025-06-09 10:12:45 +0530

    Add files via upload

---
+++ JobApplicationPortal/JobApplicationPortal/Controllers/JobApplicationController.cs
@@ -0,0 +1,51 @@
+using JobApplicationPortal.Models;
+using JobApplicationPortal.ViewModels;
+using JobApplicationPortal.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace JobApplicationPortal.Controllers
+{
+    [Authorize]
+    public class JobApplicationController : Controller
+    {
+        private readonly IJobService _jobService;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public JobApplicationController(IJobService jobService, UserManager<ApplicationUser> userManager)
+        {
+            _jobService = jobService;
+            _userManager = userManager;
+        }
+        [Authorize(Roles = "Recruiter")]
+        public async Task<IActionResult> Applications(int jobId)
+        {
+            var applications = await _jobService.GetApplicationForRecruiterAsync(_userManager.GetUserId(User));
+            return View(applications);
+        }
+        [HttpPost]
+        [Authorize(Roles = "Recruiter")]
+
+        public async Task<IActionResult> UpdateStatus(int applicationId, ApplicationStatus status)
+        {
+            await _jobService.UpdateApplicationStatusAsync(applicationId, status);
+            return RedirectToAction(nameof(Applications));
+        }
+
+        public async Task<IActionResult> MyApplications()
+        {
+            var userId = _userManager.GetUserId(User);
+            var applications = await _jobService.GetUserApplicationsAsync(userId);
+            return View(applications);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Apply(int jobId)
+        {
+            var userId = _userManager.GetUserId(User);
+            var result = await _jobService.ApplyForJobAsync(jobId, userId);
+            return RedirectToAction(nameof(MyApplications));
+        }
+    }
+}
